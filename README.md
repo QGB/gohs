87d3003b1dafffc609386004132de88936ce41b7
qgb <qgb>
2021-06-02 09:55:23 +0800

    add README.md LICENSE

---
+++ README.md
@@ -0,0 +1,22 @@
+usage:
+  `gohs [-l [address]:port]`
+
+example:
+  ```$ curl 127.0.0.1:3000/ls%20-al
+[]string{"ls", "-al"}
+total 12372
+drwxrwxrwx 3 root root    4096 Jun  2 09:45 .
+drwxrwxrwx 4 root root    4096 Jun  1 21:48 ..
+-rwxrwxrwx 1 qgb  qgb       50 Jun  2 08:52 build_win.sh
+drwxrwxr-x 8 qgb  qgb     4096 Jun  2 08:53 .git
+-rw-rw-r-- 1 qgb  qgb       15 Jun  2 08:52 .gitignore
+-rwxrwxrwx 1 root root 6323788 Jun  2 09:32 gohs
+-rwxrwxrwx 1 root root 6302208 Jun  2 09:19 gohs.exe
+-rwxrwxrwx 1 root root     116 Jun  1 20:47 go.mod
+-rwxrwxrwx 1 qgb  qgb      221 Jun  2 08:52 go.sum
+-rwxrwxrwx 1 qgb  qgb     1068 Jun  2 09:30 hs.go
+-rw-rw-r-- 1 qgb  qgb     1060 Jun  2 09:46 LICENSE
+-rwxrwxrwx 1 root root      94 Jun  2 09:51 README.md
+  
+  ```
+  
--- hs.go
+++ hs.go
@@ -1,77 +1,49 @@
 package main
 
 import (
-    "fmt"
-    "bytes"
-    "net/http"
-    //"regexp"
-     "os/exec"
-     "github.com/google/shlex" //go install github.com/google/shlex@latest
-    
+	"bytes"
+	"flag"
+	"fmt"
+	"github.com/google/shlex" //go install github.com/google/shlex@latest
+	"net/http"
+	"os/exec"
 )
 
-func hello(w http.ResponseWriter, q *http.Request) {
-   app := q.URL.Path[1:]
-   tokens, _ := shlex.Split(app)
-   fmt.Fprintf(w,"%#v\n", tokens)
-   cmd := exec.Command(tokens[0],tokens[1:]...)
+func eval_cmd(w http.ResponseWriter, q *http.Request) {
+	app := q.URL.Path[1:]
+	tokens, _ := shlex.Split(app)
+	fmt.Fprintf(w, "%#v\n", tokens)
+	cmd := exec.Command(tokens[0], tokens[1:]...)
 
-  var outb, errb bytes.Buffer
-  cmd.Stdout = &outb
-  cmd.Stderr = &errb
-  err := cmd.Run()
-  if err != nil {
-     fmt.Fprintf(w,"%#v\n", err)
-     //return
-  }
-  fmt.Fprintf(w, outb.String()+"\n"+errb.String()+"\n")  
-    
-    
-   /* 
-   
-  fmt.Println("out:", outb.String(), "err:", errb.String())
-    fmt.Fprintf(w, q.URL.Path[1:]+"\n"+string(out)+"\n")  
-       out, _ := ioutil.ReadAll(cmd.Stdout)
-    
+	var outb, errb bytes.Buffer
+	cmd.Stdout = &outb
+	cmd.Stderr = &errb
+	err := cmd.Run()
+	if err != nil {
+		fmt.Fprintf(w, "%#v\n", err)
+	}
+	fmt.Fprintf(w, outb.String()+"\n"+errb.String()+"\n")
 
-  if (err != nil) {
-       fmt.Fprintln(w, err.String())
-       return
-    } 
-   
-   
-    cmd, err := exec.Run(app, []string{app, "-l"}, nil, "", exec.DevNull, exec.Pipe, exec.Pipe)
-
-    if (err != nil) {
-       fmt.Fprintln(w, err.String())
-       return
-    }
-
-    var b bytes.Buffer
-    io.Copy(&b, cmd.Stdout)
-    fmt.Println(b.String())
-
-    cmd.Close()
-
-   out, _ := exec.Command(q.URL.Path[1:]).Output()
-  
-    fmt.Fprintf(w, q.URL.Path[1:]+"\n"+string(out)+"\n")
-    */
 }
 
 func headers(w http.ResponseWriter, req *http.Request) {
-
-    for name, headers := range req.Header {
-        for _, h := range headers {
-            fmt.Fprintf(w, "%v: %v\n", name, h)
-        }
-    }
+	for name, headers := range req.Header {
+		for _, h := range headers {
+			fmt.Fprintf(w, "%v: %v\n", name, h)
+		}
+	}
 }
 
 func main() {
-    
-    http.HandleFunc("/", hello)
-    http.HandleFunc("/headers", headers)
+	listen := flag.String("l", ":3000", "http listen [address]:port")
+	flag.Parse()
 
-    http.ListenAndServe(":8090", nil)
+	http.HandleFunc("/", eval_cmd)
+	http.HandleFunc("/headers", headers)
+
+	fmt.Println("HTTP Server eval_cmd listening at", *listen)
+	if err := http.ListenAndServe(*listen, nil); err != nil {
+		panic(err) // failure/timeout shutting down the server gracefully
+	}
+
 }
